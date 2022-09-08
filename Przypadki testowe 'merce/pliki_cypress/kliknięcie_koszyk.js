136b028b00740e83b8f7bee305aa5d5b8a0a6920
Kamil <97409624+1limak2@users.noreply.github.com>
2022-09-08 22:44:23 +0200

    Delete Przypadki testowe 'merce directory

--- Przypadki testowe 'merce/pliki_cypress/kliknięcie_koszyk.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-describe("E2E - Akcja dodania do koszyka", () => {
-    it("Kliknięcie w element", () => {
-        cy.visit("https://rekrutacja.dev.merce.com/samsung-galaxy-s21-ultra")
-        cy.contains("span", "Dodaj do koszyka").click({force: true});
- 
-    })
-})
